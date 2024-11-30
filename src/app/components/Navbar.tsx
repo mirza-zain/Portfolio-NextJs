d80835d7239ad5a42cbf75860258643c951e673c
mirza-zain <mirzazain269@gmail.com>
2024-12-01 01:13:28 +0500

    Navbar Created

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const Navbar = () => {
+    const path = usePathname()
+    const [mobVisible, setMobVisible] = useState(false)
+
+    const toggleButton = () => { setMobVisible(!mobVisible) }
+
+  return (
+    <>
+      <div className='h-16 w-full flex text-white justify-between p-5 items-center'>
+        <h1 className='font-pacifico text-3xl text-[#E40037]'>MirzaZain</h1>
+        {mobVisible ? (
+            <i className="ri-close-fill text-2xl font-bold z-50" onClick={toggleButton}></i>
+          ) : (
+            <i className="ri-menu-4-line text-2xl font-bold" onClick={toggleButton}></i>
+          )}
+          <div className="hidden md:flex" >
+              <nav className="flex gap-6 font-poppins text-lg font-bold" >
+                  <Link href="/" className = {path === "/" ? "text-[#E40037]" : "text-white"} >Home</Link>
+                  <Link href="/about" className = {path === "/about" ? "text-[#E40037]" : "text-white"} >About</Link>
+                  <Link href="/projects" className = {path === "/projects" ? "text-[#E40037]" : "text-white"} >Projects</Link>
+                  <Link href="/contact" className = {path === "/contact" ? "text-[#E40037]" : "text-white"} >Contact</Link>
+              </nav>
+          </div>
+          <div className='hidden md:flex gap-5'>
+            <a className='text-xl text-white hover:text-[#E40037] mx-3' href="#" target='_blank' title='instagram'><i className="ri-instagram-line"></i></a>
+            <a className='text-xl text-white hover:text-[#E40037] mx-3' href="#" target='_blank' title='linkedin'><i className="ri-linkedin-fill"></i></a>
+            <a className='text-xl text-white hover:text-[#E40037] mx-3' href="#" target='_blank' title='github'><i className="ri-github-fill"></i></a>
+          </div>
+      </div>
+      {mobVisible && (
+        <div className='w-full h-screen bg-black flex flex-col justify-center items-center fixed md:hidden'>
+          <nav className="flex flex-col justify-center items-center gap-8 font-poppins text-lg font-bold" >
+            <Link href="/" className = {path === "/" ? "text-[#E40037]" : "text-white"} >Home</Link>
+            <Link href="/about" className = {path === "/about" ? "text-[#E40037]" : "text-white"} >About</Link>
+            <Link href="/projects" className = {path === "/projects" ? "text-[#E40037]" : "text-white"} >Projects</Link>
+            <Link href="/contact" className = {path === "/contact" ? "text-[#E40037]" : "text-white"} >Contact</Link>
+          </nav>
+        </div>
+      )
+      }
+    </>
+  )
+}
+
+export default Navbar
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import 'remixicon/fonts/remixicon.css'
+import Navbar from "./components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,8 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={"m-0 p-0 h-full w-full flex flex-col bg-black "+`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
